5d3d02e74296dca1bec1096df38508715cc7c408
dRrankoi <vladislav15679@gmail.com>
2021-09-25 21:59:40 +0300

    First

---
+++ ConsoleApp1/ConsoleApp1/Adress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Adress
+    {
+        public string country, city, street;
+        public int index, house, apartment;
+
+        public Adress()
+        {
+            Index = index;
+
+            Country = country;
+
+            City = city;
+
+            Street = street;
+
+            House = house;
+
+            Apartment = apartment;
+        }
+        private static int Index { get; set; }
+        private static string Country { get; set; }
+        private static string City { get; set; }
+        private static string Street { get; set; }
+        private static int House { get; set; }
+        private static int Apartment { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adress adress1 = new Adress() { index = 09102, country = "Poland", city = "Krakov", street = "Pinush", house = 2, apartment = 63 };
+
+            //Console.WriteLine($"{adress1.index}, {adress1.country}, {adress1.city}, {adress1.street}, {adress1.house}, {adress1.apartment}");
+
+            double s1, s2;
+
+
+
+            Console.WriteLine("Введите значение первой стороны: ");
+
+            s1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Введите значение второй стороны: ");
+
+            s2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(s1, s2);
+
+            Console.WriteLine($"Площадь: {rectangle.Area}, периметр: {rectangle.Perimeter} ");
+
+        }
+    }
+}
